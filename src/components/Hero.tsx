0597f32a5a711b7aa454903ffb85bb8ce608d766
SaileshMrzn <shailesh.mrzn@gmail.com>
2025-06-25 22:55:12 +0545

    basic home page animation structure

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,54 +1,119 @@
 "use client";
 
 import { useState } from "react";
-import { motion, AnimatePresence, useAnimation } from "motion/react";
+import { motion, Easing } from "motion/react";
 
 export default function HeroSection() {
-  const controls = useAnimation();
   const [clicked, setClicked] = useState(false);
 
-  const handleClick = () => {
-    console.log("clicke");
-    setClicked(!clicked);
-    controls.start({
-      clipPath: [
-        "polygon(34% 63%, 100% 43%, 100% 100%, 0 100%, 0 52%)", // start
-        "polygon(34% 63%, 100% 43%, 100% 100%, 0 100%, 0 0)", // mid state
-        "polygon(34% 63%, 100% 43%, 100% 100%, 0 100%, 100% 43%)", // final
-      ],
+  const textVariants = {
+    initial: { y: 0, opacity: 1, scale: 1 },
+    animate: {
+      y: -40,
+      opacity: 0,
+      scale: 0.7,
       transition: {
-        duration: 1.2,
-        ease: [0.68, -0.55, 0.265, 1.55], // easeOutBack
+        duration: 0.2,
+        ease: "easeOut" as Easing,
+        delay: 0.55,
       },
-    });
+    },
+  };
+
+  const clipPathVariants = {
+    initial: {
+      clipPath: "polygon(50% 54%, 100% 40%, 100% 100%, 0 100%, 0 40%)",
+    },
+    animate: {
+      clipPath: [
+        "polygon(50% 54%, 100% 40%, 100% 100%, 0 100%, 0 40%)",
+        "polygon(50% 0%, 100% 40%, 100% 100%, 0 100%, 0 40%)",
+        "polygon(50% 0%, 100% 0%, 100% 100%, 0 100%, 0 0%)",
+        "polygon(50% 54%, 100% 40%, 100% 100%, 0 100%, 0 40%)",
+      ],
+
+      transition: {
+        duration: 1.5,
+        ease: [0.68, -0.55, 0.265, 1.55] as Easing, // easeOutBack
+      },
+    },
+  };
+
+  const uploadBtnVariants = {
+    initial: { y: 0 },
+    animate: {
+      y: -120,
+      transition: { duration: 0.5, ease: "easeInOut" as Easing, delay: 0.5 },
+    },
+  };
+
+  const textBoxVariants = {
+    initial: { y: -200, opacity: 0 },
+    animate: {
+      y: -60,
+      opacity: 1,
+      transition: {
+        duration: 1,
+        ease: [0, 0, 0.265, 1.55] as Easing,
+        delay: 0.5,
+      },
+    },
   };
 
   return (
     <div className="h-[90dvh] border-2 mt-10 relative">
       <motion.div
-        animate={controls}
-        initial={{
-          clipPath: "polygon(34% 63%, 100% 43%, 100% 100%, 0 100%, 0 52%",
-        }}
+        variants={clipPathVariants}
+        animate={clicked ? "animate" : "initial"}
+        initial="initial"
         className="bg-red-400 h-full w-ful"
       />
-      <div className="absolute top-50 left-[50%] transform: translate-x-[-50%]">
-        Hello {"  "}
+
+      <div className="absolute bottom-10 left-[50%] transform: translate-x-[-50%]">
         <button
-          onClick={handleClick}
+          onClick={() => setClicked(!clicked)}
           className="px-4 py-2 bg-blue-500 text-white rounded"
         >
           Animate Clip Path
         </button>
-        <motion.div
-          variants={{
-            initial: { opacity: 1, rotate: 0 },
-            transition: { opacity: 0 },
-          }}
-          className="mt-10"
+      </div>
+
+      <div className="absolute top-30 left-[50%] transform: translate-x-[-50%]">
+        <motion.h1
+          variants={textVariants}
+          initial="initial"
+          className="text-[4rem]"
+          animate={clicked ? "animate" : "initial"}
         >
-          Animate text........
-        </motion.div>
+          Finding Jobs Made Easier
+        </motion.h1>
+
+        <motion.p
+          variants={textVariants}
+          initial="initial"
+          className="text-center"
+          animate={clicked ? "animate" : "initial"}
+        >
+          Get personalized job recommendations
+        </motion.p>
+
+        <motion.p
+          variants={uploadBtnVariants}
+          initial="initial"
+          animate={clicked ? "animate" : "initial"}
+          className="text-center mt-4"
+        >
+          Upload btn
+        </motion.p>
+
+        <motion.p
+          variants={textBoxVariants}
+          initial="initial"
+          animate={clicked ? "animate" : "initial"}
+          className="text-center mt-4"
+        >
+          Text Field...
+        </motion.p>
       </div>
     </div>
   );
