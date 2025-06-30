6c4523a80176bdde506211c686328a601f88467f
SaileshMrzn <shailesh.mrzn@gmail.com>
2025-06-30 22:39:39 +0545

    upload file, file uploaded animation, submit clicked animation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,9 +2,21 @@
 
 import { useState } from "react";
 import { motion, Easing } from "motion/react";
+import FilePicker from "./custom/FilePicker";
+import { Textarea } from "./ui/textarea";
+import { Button } from "./ui/button";
 
 export default function HeroSection() {
-  const [clicked, setClicked] = useState(false);
+  const [files, setFiles] = useState<File[]>([]);
+  const [submitActive, setSubmitActive] = useState(false);
+  const [animationComplete, setAnimationComplete] = useState(false);
+
+  const handleFileUpload = (files: File[]) => {
+    setFiles(files);
+    console.log(files);
+  };
+
+  const [startAnimation, setStartAnimation] = useState(false);
 
   const textVariants = {
     initial: { y: 0, opacity: 1, scale: 1 },
@@ -34,23 +46,44 @@ export default function HeroSection() {
 
       transition: {
         duration: 1.5,
-        ease: [0.68, -0.55, 0.265, 1.55] as Easing, // easeOutBack
+        ease: [0.68, -0.55, 0.265, 1.55] as Easing,
+      },
+    },
+    postSubmit: {
+      clipPath: [
+        "polygon(50% 54%, 100% 40%, 100% 100%, 0 100%, 0 40%)",
+        "polygon(50% 0%, 100% 40%, 100% 100%, 0 100%, 0 40%)",
+        "polygon(50% 0%, 100% 0%, 100% 100%, 0 100%, 0 0%)",
+        "polygon(50% 0, 100% 0, 100% 0, 0 0, 0 0)",
+      ],
+      transition: {
+        duration: 1.5,
+        ease: [0.68, -0.55, 0.265, 1.55] as Easing,
       },
     },
   };
 
   const uploadBtnVariants = {
-    initial: { y: 0 },
+    initial: { y: 20 },
     animate: {
       y: -120,
       transition: { duration: 0.5, ease: "easeInOut" as Easing, delay: 0.5 },
+    },
+    postSubmit: {
+      y: -200,
+      opacity: 0,
+      transition: {
+        ease: "easeInOut" as Easing,
+        duration: 0.5,
+        delay: 0.3,
+      },
     },
   };
 
   const textBoxVariants = {
     initial: { y: -200, opacity: 0 },
     animate: {
-      y: -60,
+      y: -100,
       opacity: 1,
       transition: {
         duration: 1,
@@ -58,63 +91,128 @@ export default function HeroSection() {
         delay: 0.5,
       },
     },
+    postSubmit: {
+      y: -200,
+      opacity: 0,
+      transition: {
+        ease: "easeInOut" as Easing,
+        duration: 0.5,
+        delay: 0.3,
+      },
+    },
   };
+
+  console.log(submitActive, "submit");
 
   return (
     <div className="h-[90dvh] border-2 mt-10 relative">
-      <motion.div
-        variants={clipPathVariants}
-        animate={clicked ? "animate" : "initial"}
-        initial="initial"
-        className="bg-red-400 h-full w-ful"
-      />
-
-      <div className="absolute bottom-10 left-[50%] transform: translate-x-[-50%]">
-        <button
-          onClick={() => setClicked(!clicked)}
-          className="px-4 py-2 bg-blue-500 text-white rounded"
-        >
-          Animate Clip Path
-        </button>
-      </div>
-
-      <div className="absolute top-30 left-[50%] transform: translate-x-[-50%]">
-        <motion.h1
-          variants={textVariants}
+      {/* clip path */}
+      {!animationComplete && (
+        <motion.div
+          variants={clipPathVariants}
+          animate={
+            submitActive ? "postSubmit" : startAnimation ? "animate" : "initial"
+          }
           initial="initial"
-          className="text-[4rem]"
-          animate={clicked ? "animate" : "initial"}
-        >
-          Finding Jobs Made Easier
-        </motion.h1>
+          className={`bg-red-400 h-full w-full`}
+          onAnimationComplete={(definition) => {
+            if (definition === "postSubmit") {
+              setAnimationComplete(true);
+            }
+          }}
+        />
+      )}
 
-        <motion.p
-          variants={textVariants}
-          initial="initial"
-          className="text-center"
-          animate={clicked ? "animate" : "initial"}
+      {/* hero content/form... */}
+      {!animationComplete && (
+        <div
+          className={`absolute top-[10%] left-[50%] transform: translate-x-[-50%] justify-center items-center flex flex-col text-center/*  */`}
         >
-          Get personalized job recommendations
-        </motion.p>
+          <motion.h1
+            variants={textVariants}
+            initial="initial"
+            className="text-[4rem]"
+            animate={startAnimation ? "animate" : "initial"}
+          >
+            Finding Jobs Made Easier
+          </motion.h1>
 
-        <motion.p
-          variants={uploadBtnVariants}
-          initial="initial"
-          animate={clicked ? "animate" : "initial"}
-          className="text-center mt-4"
-        >
-          Upload btn
-        </motion.p>
+          <motion.p
+            variants={textVariants}
+            initial="initial"
+            className="text-center"
+            animate={startAnimation ? "animate" : "initial"}
+          >
+            Get personalized job recommendations
+          </motion.p>
 
-        <motion.p
-          variants={textBoxVariants}
-          initial="initial"
-          animate={clicked ? "animate" : "initial"}
-          className="text-center mt-4"
-        >
-          Text Field...
-        </motion.p>
-      </div>
+          <motion.div
+            variants={uploadBtnVariants}
+            initial="initial"
+            animate={
+              submitActive
+                ? "postSubmit"
+                : startAnimation
+                ? "animate"
+                : "initial"
+            }
+            className="flex justify-center"
+          >
+            <FilePicker setStartAnimation={setStartAnimation} />
+          </motion.div>
+
+          <motion.div
+            variants={textBoxVariants}
+            initial="initial"
+            animate={
+              submitActive
+                ? "postSubmit"
+                : startAnimation
+                ? "animate"
+                : "initial"
+            }
+            className="text-center w-[90%]"
+          >
+            <Textarea
+              id="description"
+              placeholder="Enter job description"
+              className="min-h-24 max-h-32"
+            />
+          </motion.div>
+
+          <motion.div
+            variants={{
+              ...textBoxVariants,
+              initial: { ...textBoxVariants.initial, scale: 1 },
+              animate: {
+                ...textBoxVariants.animate,
+                y: -80,
+              },
+              click: {
+                scale: 0.9,
+                transition: { ease: "backOut", duration: 0.3 },
+              },
+            }}
+            initial="initial"
+            animate={
+              submitActive
+                ? "postSubmit"
+                : startAnimation
+                ? "animate"
+                : "initial"
+            }
+            whileTap="click"
+            className="text-center"
+          >
+            <Button
+              onClick={() => setSubmitActive(true)}
+              className="cursor-pointer"
+            >
+              Submit
+            </Button>
+          </motion.div>
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/components/custom/FilePicker.tsx
@@ -0,0 +1,87 @@
+import React, { useEffect, useRef, useState } from "react";
+import { IoCloudUploadOutline, IoCloseOutline } from "react-icons/io5";
+import { AnimatePresence, Easing, motion } from "motion/react";
+
+function FilePicker({ setStartAnimation }: { setStartAnimation: any }) {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [fileName, setFileName] = useState<string>("");
+
+  const handleDivClick = () => {
+    inputRef.current?.click();
+  };
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length > 0) {
+      setFileName(e.target.files[0].name);
+      setStartAnimation(true);
+    }
+  };
+
+  useEffect(() => {
+    if (!fileName) {
+      setStartAnimation(false);
+    }
+  }, [fileName, setStartAnimation]);
+
+  const selectedFileFade = {
+    initial: { opacity: 0, y: -40 },
+    animate: {
+      opacity: 1,
+      y: 0,
+      transition: { ease: "backInOut" as Easing, duration: 0.4 },
+    },
+    exit: {
+      opacity: 0,
+      y: -40,
+      transition: { ease: "backInOut" as Easing, duration: 0.4 },
+    },
+  };
+
+  return (
+    <div className="flex flex-col items-center gap-1">
+      <div
+        className="w-fit px-10 py-2 rounded-sm flex items-center gap-4 bg-white/80 shadow-lg border border-gray-300 hover:bg-blue-50 transition-colors cursor-pointer relative z-2"
+        onClick={handleDivClick}
+      >
+        <input
+          type="file"
+          ref={inputRef}
+          className="hidden"
+          onChange={handleFileChange}
+        />
+        <IoCloudUploadOutline size={20} className="text-blue-500" />
+        <span className="font-medium text-gray-800">Upload Resume</span>
+      </div>
+
+      <div className="w-full h-6">
+        <AnimatePresence>
+          {fileName && (
+            <motion.div
+              className="flex items-center justify-between w-full px-1"
+              variants={selectedFileFade}
+              initial="initial"
+              animate="animate"
+              exit="exit"
+            >
+              <motion.p className="text-sm underline">{fileName}</motion.p>
+
+              <motion.div
+                onClick={() => setFileName("")}
+                initial={{ opacity: 0.7, scale: 0.9 }}
+                whileHover={{
+                  opacity: 1,
+                  scale: 1,
+                  transition: { ease: "backOut" },
+                }}
+              >
+                <IoCloseOutline />
+              </motion.div>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </div>
+    </div>
+  );
+}
+
+export default FilePicker;
